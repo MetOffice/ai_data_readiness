6a69d8cb583ef95b0e0d6061224903d254c81847
john.pill <jpill@vld529.cmpd1.metoffice.gov.uk>
2024-09-13 14:59:27 +0100

    Function docs added

--- helper_functions.py
+++ helper_functions.py
@@ -6,14 +6,40 @@ CHECKLIST_FILENAME = "Data_Readiness_Checklist.json"
 # Helper functions.
 
 def load_checklist():
+    """
+    Load the checklist from a JSON file.
+
+    Returns:
+        dict: The contents of the checklist file as a dictionary.
+    """
     with open(CHECKLIST_FILENAME, "r") as file:
         return json.load(file)
 
 def save_checklist(checklist):
+    """
+    Save the checklist to a JSON file.The JSON
+    data is pretty-printed with an indentation of 4 spaces.
+
+    Args:
+        checklist (dict): The checklist data to be saved.
+    """
     with open(CHECKLIST_FILENAME, "w") as file:
         json.dump(checklist, file, indent=4)
 
 def update_values(checklist, updates):
+    """
+    Update the values in a checklist with the provided updates.
+    This function takes a checklist and a set of updates, and applies the updates to the checklist.
+    If a section in the updates exists in the checklist, it updates the corresponding keys and values.
+    If a section in the updates does not exist in the checklist, it adds the entire section to the checklist.
+
+    Args:
+        checklist (dict): The original checklist to be updated.
+        updates (dict): A dictionary containing the updates to be applied. The structure should match that of the checklist.
+
+    Returns:
+        dict: The updated checklist.
+    """
     for section, section_updates in updates.items():
         if section in checklist:
             for key, value in section_updates.items():
@@ -23,6 +49,15 @@ def update_values(checklist, updates):
     return checklist
     
 def update_checklist(b, updates):
+    """
+    Update the checklist with the provided updates and save it.
+    This function loads the current checklist from a JSON file, applies the provided updates,
+    and then saves the updated checklist back to the JSON file.
+
+    Args:
+        b: The button widget. 
+        updates (dict): A dictionary containing the updates to be applied to the checklist.
+    """
     checklist = load_checklist() # Open json file
     checklist = update_values(checklist, updates) # update the values in the Jupyter checklist
     save_checklist(checklist) # Save the updated checklist to JSON fil
