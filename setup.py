7551b7b2bcb393fddf08dfaee86a1eaa44ad561c
LewisLee26 <lewis.patrick.lee@gmail.com>
2024-11-11 11:08:44 +0000

    Created package setup and reorganised src

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aidatareadiness",
+    version="0.0.1",
+    description="Placeholder",
+    url= "https://github.com/informatics-lab/ai_data_readiness",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7, <4",
+    install_requires=[],
+)
