450d48b08ca5efd49ec0c142339b849f2342e79e
LewisLee26 <lewis.patrick.lee@gmail.com>
2024-12-10 12:59:58 +0000

    Fix source conda bug

--- setup.sh
+++ setup.sh
@@ -11,12 +11,24 @@ else
   echo "DATA_READINESS_PATH is set to: $DATA_READINESS_PATH"
 fi
 
+  # Ensure conda is initialized
+if [ -z "$(which conda)" ]; then
+  echo "Conda is not found. Please ensure conda is installed and added to your PATH."
+  exit 1
+fi
+
+# Initialize conda for bash
+eval "$(conda shell.bash hook)"
+
 # Create the conda environment from the YML file
 echo "Creating conda environment 'data_readiness'..."
 conda env create --file envs/requirements.yml
 
+# # Activate the conda environment
+# source activate data_readiness || conda activate data_readiness
+
 # Activate the conda environment
-source activate data_readiness || conda activate data_readiness
+conda activate data_readiness
 
 # Set up the IPython kernel
 echo "Setting up IPython kernel for Jupyter Lab..."
